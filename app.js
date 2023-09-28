cd22aa9dbcdff0195e16ce8ced8e2b10785f0a8c
Daniel Mulato <ispdannielfisico@gmail.com>
2023-09-28 14:24:31 -0300

    Teste Completo do Pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Seja bem Vindo!'));
+app.get('/', (req, res) => res.send('Teste Completo do Pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
